ca1cecec8499a8860ab1ed94bc8acccbada3e2e3
Maksim Panchenko <maks@fb.com>
2025-12-15 23:06:58 -0800

    [WIP][BOLT][AArch64] Add experimental function relaxation pass
    Replacement for LongJmp main pass.

--- bolt/test/AArch64/veneer-bolt.s
+++ bolt/test/AArch64/veneer-bolt.s
@@ -1,11 +1,17 @@
-## Check that llvm-bolt correctly handles veneers in lite mode.
+## Check that llvm-bolt handles code larger than 256MB.
+## Additionally, check veneers: no double veneers in lite mode and proper names
+## for BOLT-introduced veneers.
 
 # RUN: llvm-mc -filetype=obj -triple aarch64-unknown-unknown %s -o %t.o
 # RUN: link_fdata %s %t.o %t.fdata
 # RUN: llvm-strip --strip-unneeded %t.o
 # RUN: %clang %cflags %t.o -o %t.exe -nostdlib -Wl,-q
-# RUN: llvm-bolt %t.exe -o %t.bolt --lite=1 --data %t.fdata \
+# RUN: llvm-bolt %t.exe -o %t.bolt --relax-exp --lite=0
+# RUN: llvm-bolt %t.exe -o %t.bolt --relax-exp --lite=1 --data %t.fdata \
 # RUN:   --print-normalized 2>&1 | FileCheck %s --check-prefix=CHECK-VENEER
+# RUN: llvm-bolt %t.exe -o %t.bolt --relax-exp --hot-functions-at-end --lite=0 \
+# RUN:   --data %t.fdata
+# RUN: llvm-objdump -d %t.bolt | FileCheck %s
 
 ## Constant islands at the end of functions foo(), bar(), and _start() make each
 ## one of them ~112MB in size. Thus the total code size exceeds 300MB.
@@ -43,6 +49,12 @@ hot:
 # CHECK-VENEER-LABEL: Binary Function "hot"
 # CHECK-VENEER: bl
 # CHECK-VENEER-SAME: {{[[:space:]]foo[[:space:]]}}
+
+## Check that BOLT-introduced veneers have proper names.
+# CHECK-LABEL: <hot>:
+# CHECK-NEXT: bl {{.*}} <__AArch64ADRPThunk_foo>
+# CHECK-NEXT: bl {{.*}} <__AArch64Thunk_bar>
+# CHECK-NEXT: bl {{.*}} <_start>
 
   .global _start
   .type _start, %function
